9e749a9fc8131382417b42fbb3ad27d49fafa32b
cesartalves <cesartorralvoa@hotmail.com>
2019-08-14 13:32:32 -0300

    handle authenticyexception on vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,8 @@ class VotesController < ApplicationController
 
     before_action :authenticate_user!
 
+    rescue_from ActionController::InvalidAuthenticityToken, with: :handle_csrf_error
+
     def vote
         movie_id = params[:movie_id]
         vote_type = params[:vote_type]
@@ -9,9 +11,14 @@ class VotesController < ApplicationController
         if compute_vote(movie_id, vote_type)
             flash[:notice] = "Voto computado!"
         else
-            flash[:error] = "Houve algum erro computando seu voto :("
+            flash[:error] = "Houve algum erro computando seu voto. :("
         end
 
+        redirect_to root_url
+    end
+
+    def handle_csrf_error
+        flash[:error] = "Falha em checar autenticidade do voto. Tente novamente."
         redirect_to root_url
     end
 
