010efb82685c56306b02185bc03fbdbce7c7643e
cesartalves <cesartorralvoa@hotmail.com>
2019-08-14 12:19:36 -0300

    add host to production mailer smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    :host => 'tray-imdb.herokuapp.com',
     :authentication => :plain,
     :address => "smtp.mailgun.org",
     :port => 587,
