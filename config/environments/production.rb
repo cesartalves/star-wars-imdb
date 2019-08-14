9d887de9b56a896a2088ba55350d43c02d018731
cesartalves <cesartorralvoa@hotmail.com>
2019-08-14 12:19:36 -0300

    add host to production default_url_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,9 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'tray-imdb.herokuapp.com' }
+  
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
     :address => "smtp.mailgun.org",
